a42f00107006f1f0260c2953a0ebb880dcfee1b3
nkwangleiGIT <nkwanglei@126.com>
2024-09-19 13:36:15 +0800

    feat: support to search docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -46,6 +46,24 @@ const config: Config = {
     'plugin-image-zoom',
   ],
 
+  themes: [
+    // ... Your other themes.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        docsRouteBasePath: '/',
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+      }),
+    ],
+  ],
+
   presets: [
     [
       'classic',
